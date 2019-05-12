fd42bd24b1a326d91b807792da66c675a3dcc445
ivonamilutinovic <ivona.milutinovic95@gmail.com>
2019-05-12 03:17:06 +0200

    zavrsen moj deo funkcije smisliIme :D

--- generator.cpp
+++ generator.cpp
@@ -1,7 +1,7 @@
 #include "generator.h"
 #include "ui_generator.h"
 
-Generator::Generator(Board * board, QWidget *parent) :
+Generator::Generator(QWidget *parent) :
 
     QMainWindow(parent),
     ui(new Ui::Generator),
@@ -25,13 +25,31 @@ Generator::Generator(Board * board, QWidget *parent) :
 
 void smisliNaziv(Board* board){
 
+   // nalazimo polje na kome se nalazi crni kralj
    Field blackKing = board->findKing(Color::black);
    // u allowedPosition nam se nalaze susedna polja za kralja
    std::vector<Field> allowedPosition = blackKing.adjacentFields();
    for(Field field: allowedPosition){
-       if(board->get(field.rank(),field.file())->isColor(Color::black)){
-            // proveravamo da li neka bela napada tu crnu
+       if(board->get(field.rank(),field.file())->color() == Color::black){
+           // proveravamo da li neka bela napada tu crnu
 
+           // prolazimo kroz sve bele figure na tabli
+           for(int i = 1; i < 9; i++)
+                for(char j = 'a'; j < 'h' + 1; j++)
+                    if(board->get(i,j)->color() == Color::white){
+
+                        // zovemo metod legalMoves za sve bele figure kako bismo proverili
+                        // da li neka bela napada crne koje su oko kralja
+                        std::vector<Field> moves = board->legalMoves(Field(i,j));
+                        // proveravamo da li legalMoves vraca polje koje je jednako poljima na kome se nalaze crne figure oko kralja
+                        for(Field position : moves){
+
+                            if(position == field){
+                                //Dejan radi nesto
+                            }
+                        }
+
+                    }
        }
    }
 
